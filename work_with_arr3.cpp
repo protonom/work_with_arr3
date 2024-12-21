ca7be67c1e3a0436ac6b9c946cb50807881dc36e
gotonom <gotonom@gmail.com>
2024-12-22 00:19:30 +0300

    dz

--- work_with_arr3.cpp
+++ work_with_arr3.cpp
@@ -1,23 +1,31 @@
 ﻿#include <iostream>
 
-void fill_arr(int* arr, int size) {
+void fill_arr(int* arr, int size) 
+{
 	int i = 0;
-	for (i; i < size; i++) {
-		arr[i] = i + 1;
-	}
-}
-void show_arr(int* arr, int size) {
-	int i = 0;
-	for (i; i < size; i++) {
-		std::cout << arr[i];
+	for (i; i < size; i++) 
+	{
+		arr[i] = i * 5 + 100;
 	}
 }
 
+
+void show_arr(int* arr, int size) 
+{
+	int i = 0;
+	for (i; i < size; i++) 
+	{
+		std::cout << arr[i] << ", ";
+	}
+}
+
+
 int main()
 {
-	const int size = 10;
+	const int size = 20;
 	int arr[size];
 	fill_arr(arr, size);
 	show_arr(arr, size);
+	
 }
 
