ee50538eaffa118faabc09bf062a649a92798596
gotonom <gotonom@gmail.com>
2024-12-21 15:38:26 +0300

    top

---
+++ work_with_arr3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+void fill_arr(int* arr, int size) {
+	int i = 0;
+	for (i; i < size; i++) {
+		arr[i] = i + 1;
+	}
+}
+void show_arr(int* arr, int size) {
+	int i = 0;
+	for (i; i < size; i++) {
+		std::cout << arr[i];
+	}
+}
+
+int main()
+{
+	const int size = 10;
+	int arr[size];
+	fill_arr(arr, size);
+	show_arr(arr, size);
+}
+
